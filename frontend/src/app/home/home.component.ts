1eb254f8bf397b63ee72c6b970d4f5d1a74c0ec8
LastChance89 <ksmitw@gmail.com>
2020-07-12 20:29:12 -0400

    updates

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import { Card } from '../model/card';
 
 @Component({
   selector: 'app-home',
@@ -12,4 +14,13 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
   }
 
+  
+  card1 : Card = new Card("defect","Card 1",1,"Test User 1","Card 1", 1); 
+  card2 : Card = new Card("enhancement","Card 2",1,"Test User 2","Card 2", 2); 
+  cardList = [this.card1, this.card2];
+
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.cardList, event.previousIndex, event.currentIndex);
+  }
 }
